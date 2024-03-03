045761832cf1fe6b2a61400cbaf2d6592a8a1cc7
Alan <alanfga1@gmail.com>
2024-03-03 15:24:18 -0700

    creating routes and CRUD for todo

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Router } from "express";
 import path from "path";
 
 interface Options {
   port: number;
+  routes: Router
   public_path?: string;
 }
 
@@ -10,18 +11,28 @@ export class Server {
   private app = express();
   private readonly port: Number;
   private readonly publiPath: string;
+  private readonly routes: Router
 
 constructor(options: Options) {
-    const {port, public_path = 'public'} = options;
+    const {port, public_path = 'public', routes} = options;
+
     this.port = port;
     this.publiPath = public_path;
+    this.routes = routes;
 }
 
   async start() {
     //*midleware
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({extended: true}));
+
     // * Public folder
     this.app.use(express.static(this.publiPath));
 
+    //* Routes
+    this.app.use( this.routes );
+
+    //* SPA
     this.app.get("*", (req, res) => {
       const indexPath = path.join(__dirname + `../../../${this.publiPath}/index.html`);
       res.sendFile(indexPath);
